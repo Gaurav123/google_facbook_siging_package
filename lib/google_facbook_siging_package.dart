9293c742a231c36ca6af4e0eff89f776ab7f29f4
@GSatriina <gaurav.sharma@atriina.com>
2021-10-29 18:28:31 +0530

    Initial commit

---
+++ lib/google_facbook_siging_package.dart
@@ -0,0 +1,4 @@
+library google_facbook_siging_package;
+
+export 'facebook_signing_in.dart';
+export 'google_siging_in.dart';
---
+++ test/google_facbook_siging_package_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:google_facbook_siging_package/google_facbook_siging_package.dart';
+
+void main() {
+
+}
